b960a1185dca4fbeea306f115d253f4825921aff
Riley Brown <brown.riley.c@gmail.com>
2019-03-25 14:25:56 -0400

    More boiler plate in place

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+var express = require("express");
+// var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI ||process.env.mongoLocalhost;
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Routes
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
